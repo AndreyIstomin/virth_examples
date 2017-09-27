2149db08921c79251a96bf767f34ae3f12737196
Andrey Istomin <istomin_andrey@outlook.com>
2017-09-28 01:27:45 +0300

    Sorting analysis

---
+++ sorting.py
@@ -0,0 +1,47 @@
+import random
+
+def straight_insertion(list_to_sort):
+
+    n = len(list_to_sort)
+    list_to_sort.append(0)
+
+    for i in xrange(0, n):
+
+        x = list_to_sort[-1] = list_to_sort[i]
+
+        j = i
+
+        while x > list_to_sort[j + 1]:
+
+            list_to_sort[j] = list_to_sort[j + 1]
+            j += 1
+
+        list_to_sort[j] = x
+
+
+def unorder_range(range_size):
+
+    init_list = range(0, range_size)
+
+    return [init_list.pop(int((range_size - i) * random.random())) for i in xrange(0, range_size)]
+
+def create_list_to_sort(list_size):
+
+    return {
+        'ordered': range(0, list_size),
+        'inverted': range(list_size - 1, -1),
+        'random': [int(list_size * random.random()) for i in xrange(list_size)]
+    }
+
+if __name__ == '__main__':
+
+    list_size = []
+
+    list_to_sort = \
+        [
+            # 'ordered':
+        ]
+
+    sorting = [straight_insertion]
+
+    # TO BE CONTINUED...
